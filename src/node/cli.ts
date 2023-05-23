f7a31b1692cd253114aec002657fe75855b22168
zzydev <4amzzydev@gmail.com>
2023-05-23 19:30:01 +0800

    feat: init unit test and e2e test

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 import { build } from './build';
 import { createDevServer } from './dev';
 
-const cli = cac('island').version('0.0.1').help();
+const cli = cac('awili').version('0.0.1').help();
 
 cli.command('dev [root]', 'start dev server').action(async (root: string) => {
   const server = await createDevServer(root);
