58b51ef282a6c969edfcfbc16c67d34b411d03a9
zzydev <4amzzydev@gmail.com>
2023-07-01 01:21:06 +0800

    test: add code highlight test

--- src/node/__test__/md.test.ts
+++ src/node/__test__/md.test.ts
@@ -4,14 +4,19 @@ import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
 import { rehypePluginPreWrapper } from '../../node/plugin-mdx/rehypePlugins/preWrapper';
+import { rehypePluginCodeHighLight } from '../../node/plugin-mdx/rehypePlugins/codeHighLight';
+import shiki from 'shiki';
 
-describe('Markdown compile cases', () => {
+describe('Markdown compile cases', async () => {
   // 初始化 processor
   const processor = unified()
     .use(remarkParse)
     .use(remarkRehype)
     .use(rehypeStringify)
-    .use(rehypePluginPreWrapper);
+    .use(rehypePluginPreWrapper)
+    .use(rehypePluginCodeHighLight, {
+      highlighter: await shiki.getHighlighter({ theme: 'github-dark-dimmed' })
+    });
 
   test('Compile title', async () => {
     const mdContent = '# 123';
@@ -30,13 +35,9 @@ describe('Markdown compile cases', () => {
   test('Compile code block', async () => {
     const mdContent = '```js\nconsole.log("awili");\n```';
     const result = processor.processSync(mdContent);
-    // <div class="language-js">
-    // <span class="lang"> js </span>
-    // <pre><code>console.log("awili"); </code></pre>
-    // </div>
     expect(result.value).toMatchInlineSnapshot(`
-      "<div class=\\"language-js\\"><span class=\\"lang\\">js</span><pre><code class=\\"language-js\\">console.log(\\"awili\\");
-      </code></pre></div>"
+      "<div class=\\"language-js\\"><span class=\\"lang\\">js</span><pre class=\\"shiki github-dark-dimmed\\" style=\\"background-color: #22272e\\" tabindex=\\"0\\"><code><span class=\\"line\\"><span style=\\"color: #ADBAC7\\">console.</span><span style=\\"color: #DCBDFB\\">log</span><span style=\\"color: #ADBAC7\\">(</span><span style=\\"color: #96D0FF\\">\\"awili\\"</span><span style=\\"color: #ADBAC7\\">);</span></span>
+      <span class=\\"line\\"></span></code></pre></div>"
     `);
   });
 });
