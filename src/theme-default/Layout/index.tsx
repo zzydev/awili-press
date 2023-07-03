23dce359f2ce581e8207db717dd6fc3e613e6e2a
zzydev <4amzzydev@gmail.com>
2023-07-04 06:19:00 +0800

    feat: add unocss

--- src/theme-default/Layout/index.tsx
+++ src/theme-default/Layout/index.tsx
@@ -1,9 +1,12 @@
 import { Content } from '@runtime';
+import 'uno.css';
 
 export function Layout() {
   return (
     <div>
-      <h1>Layout</h1>
+      <h1 p="6" m="2" className="text-red-500">
+        Layout
+      </h1>
       <Content />
     </div>
   );
